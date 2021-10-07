a12f40020ac8a12a82a5fada594187daa82aa564
Kory Draughn <korydraughn@renci.org>
2021-10-07 15:46:37 -0400

    It compiles against 4.3.0!

--- install-Rcpp.R
+++ install-Rcpp.R
@@ -1,2 +1,3 @@
 # must be installed as irods linux user, so tests can run as irods linux user
 install.packages('Rcpp', repos='http://cran.us.r-project.org')
+install.packages('knitr', repos='http://cran.us.r-project.org')
