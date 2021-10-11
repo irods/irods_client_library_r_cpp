8262ea322749842ba859aeb7b6545bf5b6995fb6
martinschobben <martin.schobben@hotmail.com>
2021-10-11 11:15:46 +0200

    shell script

---
+++ build-rirods.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# build image
+sudo docker image build --tag rirods:build .
+
+# local source and library paths
+SRCPATH=$(R -e "getwd()"| grep /)
+LIBPATH=$(R -e ".libPaths()[1]" | grep /)
+
+# devtools document
+sudo docker container run --rm --name rirods-document \
+  -v $(SRCPATH):/rirods \
+  rirods:build \
+  -e 'devtools::document()'
+
+# devtools build
+sudo docker container run --rm --name rirods-document \
+  -v $(SRCPATH):/rirods \
+  rirods:build \
+  -e 'devtools::build()'
+
+# clean src
+rm -f src/*.o && rm -f src/*.so && rm -f src/*.dll
+
